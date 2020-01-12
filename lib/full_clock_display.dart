08fc46d2521832aa07dc4205b0e0595041dde85b
Allan Caine <adcaine@gmail.com>
2020-01-12 07:50:07 -0500

    Add sketch of the FullClockDisplay class

---
+++ lib/full_clock_display.dart
@@ -0,0 +1,17 @@
+// Copyright 2019 Allan Caine. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class FullClockDisplay extends StatelessWidget {
+
+  final DateTime dateTime;
+
+  FullClockDisplay({@required this.dateTime});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
